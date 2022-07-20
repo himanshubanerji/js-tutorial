736765a7af063ca27ca7f10f1accf235ce2bef8d
Himanshu Banerji <42094721+himanshubanerji@users.noreply.github.com>
2022-07-20 08:02:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// the first javascript program
+console.log("Hello, World!");
+
+// variables can store information
+// we can use or modify that information later
+
+let name = "Himanshu"; //declared & initialised a variable
+console.log(name);
+name = "Heman";
+console.log(name); //modified the variable name
